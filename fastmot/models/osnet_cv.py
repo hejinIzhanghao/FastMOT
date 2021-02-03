8dc2d1a2988bd9354c8a8d7ff048ff25bca14017
hejinIzhanghao <zhangh.tju@outlook.com>
2021-02-03 08:56:01 +0800

    Update osnet_cv.py
    OSNet_CV::extract，输入参数为detections

--- fastmot/models/osnet_cv.py
+++ fastmot/models/osnet_cv.py
@@ -1,7 +1,11 @@
+import numpy as np
 import cv2
 
 
 class OSNet_CV:
+    metric = 'euclidean'
+    OUTPUT_LAYOUT = 512
+
     def __init__(self, config):
 
         model_cfg = config['model_cfg']
@@ -15,9 +19,23 @@ class OSNet_CV:
     def __call__(self, frame):
         return self.extract(frame)
 
-    def extract(self, frame):
-
+    def _extract(self, frame):
         blob = cv2.dnn.blobFromImage(frame, (1.0 / 255), (self.input_shape[0], self.input_shape[1]), [0, 0, 0], 1, crop=False)
         self.net.setInput(blob)
         out = self.net.forward()
         return out
+
+    def extract(self, frame, detections):
+        if len(detections) == 0:
+            return np.empty((0, self.OUTPUT_LAYOUT))
+
+        embeddings = []
+        for det in detections:
+            tlbr_ = det.tlbr.astype(np.int)
+            crop = frame[tlbr_[1]:tlbr_[3] + 1, tlbr_[0]:tlbr_[2] + 1]
+            feat = self._extract(crop)
+            embeddings.append(feat)
+
+        embeddings = np.concatenate(embeddings).reshape(-1, self.OUTPUT_LAYOUT)
+        embeddings /= np.linalg.norm(embeddings, axis=1, keepdims=True)
+        return embeddings
